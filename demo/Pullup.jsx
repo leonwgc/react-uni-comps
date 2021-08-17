06193349498d10974a3f19fcf13791e6fccfff8e
leonwgc <giantfish@126.com>
2021-08-17 15:41:22 +0800

    chore: update demo

--- demo/Pullup.jsx
+++ demo/Pullup.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Pullup, Space } from 'react-uni-comps';
-import './Pullup.less';
+import { Pullup } from 'react-uni-comps';
 
 // 第一次加载数据应该撑满容器,否则会一直拉数据知道撑满
 const pageSize = 20;
@@ -31,34 +30,20 @@ const App = () => {
   };
 
   return (
-    <>
-      <Space>
-        {/* <Pullup
-          className="pull-wrapper"
-          dataList={list}
-          fetchData={fetchData}
-          finished={finished}
-          dataRender={(data) => {
-            return <div className="item">list {data}</div>;
-          }}
-        ></Pullup> */}
-
-        <Pullup
-          style={{ height: '100vh', width: '100vw', border: 'none', margin: '16px 8px' }}
-          dataList={list}
-          finishedText="没有更多数据了!"
-          fetchData={fetchData}
-          finished={finished}
-          dataRender={(data, index) => {
-            return (
-              <div style={{ padding: '10px 0', borderBottom: '1px solid #e0e0e0' }}>
-                list {index + 1}
-              </div>
-            );
-          }}
-        ></Pullup>
-      </Space>
-    </>
+    <Pullup
+      style={{ height: '100vh', width: '100vw', border: 'none', margin: '-16px' }}
+      dataList={list}
+      finishedText="没有更多数据了!"
+      fetchData={fetchData}
+      finished={finished}
+      dataRender={(data, index) => {
+        return (
+          <div style={{ padding: '10px 0', borderBottom: '1px solid #e0e0e0' }}>
+            list {index + 1}
+          </div>
+        );
+      }}
+    ></Pullup>
   );
 };
 
