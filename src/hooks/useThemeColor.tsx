c7772a731a4954752ea959178b3133f740e24121
leonwgc <giantfish@126.com>
2021-08-26 14:32:34 +0800

    refactor: useThemeColor

--- src/hooks/useThemeColor.tsx
+++ src/hooks/useThemeColor.tsx
@@ -2,15 +2,15 @@ import React, { useContext } from 'react';
 import { ThemeContext } from 'styled-components';
 import * as colors from '../colors';
 /**
- *  获得主题色
+ *  获得主题色,主题色优先,没有配置，则取colors定义的主题色
  *
  * @export
- * @param {string} defaultColor
+ * @param {string} fallbackColor
  * @return {*}  {string}
  */
-export default function useThemeColor(defaultColor = colors.primary): string {
+export default function useThemeColor(fallbackColor = colors.primary): string {
   const theme = useContext(ThemeContext);
-  let color = defaultColor;
+  let color = fallbackColor;
   if (typeof theme.color === 'string') {
     color = theme.color;
   }
