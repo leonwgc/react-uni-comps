032733cf21d09526ac20499c82a65bddcc1fe5ac
leonwgc <giantfish@126.com>
2021-09-11 18:29:57 +0800

    refactor: fg demo

--- demo/FingerGestureElement.jsx
+++ demo/FingerGestureElement.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { FingerGestureElement, Toast } from '../src';
-import bird from './images/cat.jpg';
+import bird from './images/cat.gif';
 
 export default function App() {
   const ref = useRef();
@@ -30,7 +30,7 @@ export default function App() {
         }}
         onRotate={(e) => {
           e.preventDefault();
-          
+
           thisRef.current.rotate += e.angle;
           e.target.style.transform = `scale(${thisRef.current.scale}) rotate(${thisRef.current.rotate}deg) translate(${thisRef.current.x}px,${thisRef.current.y}px)`;
         }}
@@ -49,7 +49,7 @@ export default function App() {
           src={bird}
           width={200}
           height={200}
-          style={{ objectFit: 'scale-down', transformOrigin: 'center' }}
+          style={{ objectFit: 'scale-down', transformOrigin: 'center', border: '1px solid #eee' }}
         />
       </FingerGestureElement>
       <div>{JSON.stringify(data)}</div>
