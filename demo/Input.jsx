f3081f75387a6dbe2de6c74013c9d63b9a919148
leonwgc <giantfish@126.com>
2021-09-16 18:13:38 +0800

    feat: Input

--- demo/Input.jsx
+++ demo/Input.jsx
@@ -1,11 +1,34 @@
 import React, { useState, useref, useRef } from 'react';
-import { Space } from '../src';
+import { Space, Divider } from '../src';
+import { SoundOutlined } from '@ant-design/icons';
 import Input from '../src/Input';
 
 export default function App() {
+  const [v, setV] = useState('');
   return (
     <div className="app" style={{ margin: '20px' }}>
-      <label>name</label> <Input></Input>
+      <Divider>default</Divider>
+      <Input></Input>
+      <Divider>prefix/suffix</Divider>
+      <Input
+        value={v}
+        maxLength={10}
+        onChange={(e) => setV(e.target.value)}
+        prefix={
+          <span>
+            <SoundOutlined />
+            名字
+          </span>
+        }
+        suffix={v.length + ' / 10'}
+      ></Input>
+      <Divider>suffix</Divider>
+      <Input
+        value={v}
+        maxLength={10}
+        onChange={(e) => setV(e.target.value)}
+        suffix={v.length + ' / 10'}
+      ></Input>
     </div>
   );
 }
--- src/Input.tsx
+++ src/Input.tsx
@@ -5,58 +5,89 @@ import { getThemeColorCss } from './themeHelper';
 import * as colors from './colors';
 import clsx from 'clsx';
 
-type Props = HTMLAttributes<HTMLInputElement>;
+type Props = {
+  /** input左边内容 */
+  prefix?: React.ReactNode;
+  /** input右边内容 */
+  suffix?: React.ReactNode;
+} & HTMLAttributes<HTMLInputElement>;
 
-const StyledInput = styled.input`
-  position: relative;
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  color: #333;
-  line-height: inherit;
-  text-align: left;
-  background-color: transparent;
-  border: 0;
-  resize: none;
-  outline: none;
-  -webkit-tap-highlight-color: transparent;
-  -webkit-appearance: none;
-  box-shadow: none;
-  font-size: 14px;
-  width: 100%;
+const StyledInput = styled.div`
+  display: flex;
+  align-items: center;
 
-  &.mobile {
-    display: block;
-    line-height: 24px;
+  &.prefix {
+    input {
+      margin-left: 8px;
+    }
+  }
+
+  &.suffix {
+    input {
+      margin-right: 8px;
+    }
   }
 
   &.pc {
-    display: inline-block;
-    width: 100%;
-    padding: 4px 12px;
-    color: #000000d9;
-    font-size: 14px;
-    line-height: 1.5715;
-    background-color: #fff;
-    background-image: none;
-    border: 1px solid ${colors.border};
-    border-radius: 2px;
-    transition: all 0.3s;
-    &:hover {
-      ${getThemeColorCss('border-color')}
+    input {
+      display: inline-block;
+      padding: 4px 12px;
+      font-size: 14px;
+      line-height: 1.5715;
+      background-color: #fff;
+      background-image: none;
+      border: 1px solid ${colors.border};
+      border-radius: 2px;
+      transition: all 0.3s;
+      &:hover {
+        ${getThemeColorCss('border-color')}
+      }
     }
+  }
+  &.mobile {
+    input {
+      display: block;
+      line-height: 24px;
+    }
+  }
+  input {
+    flex: 1;
+    position: relative;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    color: #333;
+    line-height: inherit;
+    text-align: left;
+    background-color: transparent;
+    border: 0;
+    resize: none;
+    outline: none;
+    -webkit-tap-highlight-color: transparent;
+    -webkit-appearance: none;
+    box-shadow: none;
+    font-size: 14px;
+    width: 100%;
   }
 `;
 
 /** 输入框 */
 const Input = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
-  const { className, ...rest } = props;
+  const { className, prefix, suffix, ...rest } = props;
+
   return (
     <StyledInput
-      {...rest}
-      ref={ref}
-      className={clsx('uc-input', className, { mobile: isMobile(), pc: !isMobile() })}
-    ></StyledInput>
+      className={clsx('uc-input', className, {
+        mobile: isMobile(),
+        pc: !isMobile(),
+        prefix: prefix,
+        suffix: suffix,
+      })}
+    >
+      {prefix && <span className={clsx('prefix')}>{prefix}</span>}
+      <input {...rest} ref={ref} />
+      {suffix && <span className={clsx('suffix')}>{suffix}</span>}
+    </StyledInput>
   );
 });
 
