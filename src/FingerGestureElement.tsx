b612115c283bf4fc829ed079b1b80fc486a29d38
leonwgc <giantfish@126.com>
2021-09-16 11:48:18 +0800

    refactor:

--- src/FingerGestureElement.tsx
+++ src/FingerGestureElement.tsx
@@ -1,12 +1,13 @@
-import React, { useRef, useImperativeHandle } from 'react';
+import React, { useRef, useImperativeHandle, HTMLAttributes } from 'react';
 import useGesture from './hooks/useGesture';
 import { Option } from './hooks/FingerGesture';
 
 type Props = {
   children: React.ReactElement;
-} & Partial<Option>;
-
-/** 手势操作元素 */
+} & Partial<Option> &
+  HTMLAttributes<HTMLElement>;
+// TODO: omit rest props from children
+/** 手势操作元素, 非手势props透传到子元素 */
 const FingerGestureElement = React.forwardRef<HTMLElement, Props>((props, ref) => {
   const { children, ...rest } = props;
   const elRef = useRef();
--- src/SwipeAction.tsx
+++ src/SwipeAction.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useImperativeHandle, useLayoutEffect } from 'react';
+import React, { useRef, useImperativeHandle, useLayoutEffect, useCallback, useEffect } from 'react';
 import FingerGestureElement from './FingerGestureElement';
 import styled from 'styled-components';
 import useThisRef from './hooks/useThisRef';
@@ -12,8 +12,10 @@ type Props = {
   children: React.ReactNode;
   onOpen: (type: openType) => void;
   onClose: (type: openType) => void;
-  /** 点击按钮后是否自动关闭 */
+  /** 点击按钮后是否自动关闭,默认true */
   autoClose?: boolean;
+  /** 点击外部区域关闭,默认true*/
+  closeOnClickOutside?: boolean;
 };
 
 const StyledSwipeAction = styled.div`
@@ -49,27 +51,67 @@ const StyledSwipeAction = styled.div`
 
 /** SwipeAction 滑动操作 */
 const SwipeAction = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
-  const { left, right, onClose, onOpen, autoClose = true, children } = props;
+  const {
+    left,
+    right,
+    onClose,
+    onOpen,
+    autoClose = true,
+    closeOnClickOutside = true,
+    children,
+  } = props;
   const elRef = useRef();
   const thisRef = useThisRef({
     x: 0,
     onClose,
     onOpen,
+    closeOnClickOutside,
     el: null,
     leftEl: null,
     rightEl: null,
     leftWidth: 0,
     rightWidth: 0,
-    lastMoveX: 0,
     isOpen: 0,
   });
   useImperativeHandle(ref, () => elRef.current);
+
+  useEffect(() => {
+    const v = thisRef.current;
+    if (v.closeOnClickOutside) {
+      const closeHandler = (e) => {
+        if (!v.isOpen) {
+          return;
+        }
+
+        if (!v.el.contains(e.target)) {
+          startTransform('translate3d(0,0,0)');
+          v.x = 0;
+        }
+      };
+      window.addEventListener('click', closeHandler);
+
+      return () => {
+        window.removeEventListener('click', closeHandler);
+      };
+    }
+  }, []);
 
   useLayoutEffect(() => {
     thisRef.current.el = elRef.current;
     thisRef.current.leftWidth = thisRef.current.leftEl.offsetWidth;
     thisRef.current.rightWidth = thisRef.current.rightEl.offsetWidth;
   }, [thisRef]);
+
+  const startTransform = useCallback(
+    (transformStr) => {
+      const v = thisRef.current;
+      v.el.style.transition = 'transform .3s ease-in-out';
+      setTimeout(() => {
+        v.el.style.transform = `${transformStr}`;
+      });
+    },
+    [thisRef]
+  );
 
   return (
     <FingerGestureElement
@@ -79,27 +121,29 @@ const SwipeAction = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
       }}
       onClick={() => {
         if (autoClose) {
-          const v = thisRef.current;
-          v.el.style.transition = 'transform .3s ease-in-out';
-          setTimeout(() => {
-            v.el.style.transform = `translate3d(0,0,0)`;
-          });
+          startTransform('translate3d(0,0,0)');
         }
       }}
       onTouchEnd={() => {
         const v = thisRef.current;
 
+        // v.el.style.transition = 'transform .3s ease-in-out';
+
         if (v.x < 0) {
           // open right
           if (Math.abs(v.x) < v.rightWidth / 2) {
             // no more than half way
-            v.el.style.transform = `translate3d(0,0,0)`;
+            // v.el.style.transform = `translate3d(0,0,0)`;
+            startTransform('translate3d(0,0,0)');
+            v.x = 0;
             if (v.isOpen) {
               v.onClose?.('right');
               v.isOpen = 0;
             }
           } else {
-            v.el.style.transform = `translate3d(-${v.rightWidth}px,0,0)`;
+            // v.el.style.transform = `translate3d(-${v.rightWidth}px,0,0)`;
+            startTransform(`translate3d(-${v.rightWidth}px,0,0)`);
+            v.x = -1 * v.rightWidth;
             if (!v.isOpen) {
               v.onOpen?.('right');
               v.isOpen = 1;
@@ -108,13 +152,17 @@ const SwipeAction = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
         } else if (v.x > 0) {
           if (Math.abs(v.x) < v.leftWidth / 2) {
             // no more than half way
-            v.el.style.transform = `translate3d(0,0,0)`;
+            // v.el.style.transform = `translate3d(0,0,0)`;
+            startTransform('translate3d(0,0,0)');
+            v.x = 0;
             if (v.isOpen) {
               v.onClose?.('left');
               v.isOpen = 0;
             }
           } else {
-            v.el.style.transform = `translate3d(${v.leftWidth}px,0,0)`;
+            // v.el.style.transform = `translate3d(${v.leftWidth}px,0,0)`;
+            startTransform(`translate3d(${v.leftWidth}px,0,0)`);
+            v.x = v.leftWidth;
             if (!v.isOpen) {
               v.onOpen?.('left');
               v.isOpen = 1;
@@ -125,7 +173,6 @@ const SwipeAction = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
       onPressMove={(e) => {
         e.preventDefault();
         const v = thisRef.current;
-        v.lastMoveX = e.deltaX;
         v.x += e.deltaX;
         // x<0:swipe left & show right
         if (v.x < 0 && Math.abs(v.x) < v.rightWidth) {
