1dcb46a5ed45ba4be3db52267ab55ff3c8ea1e2d
leonwgc <giantfish@126.com>
2021-10-07 10:14:52 +0800

    chore: update radio demo

--- demo/RadioGroup.jsx
+++ demo/RadioGroup.jsx
@@ -49,6 +49,39 @@ export default function App() {
           setV(v);
         }}
       ></RadioGroup>
+
+      <Divider>label/value options </Divider>
+      <RadioGroup
+        options={options1}
+        value={v}
+        onChange={(v) => {
+          console.log(v);
+          setV(v);
+        }}
+      ></RadioGroup>
+
+      <Divider>button style </Divider>
+      <RadioGroup
+        button
+        options={options1}
+        value={v}
+        onChange={(v) => {
+          console.log(v);
+          setV(v);
+        }}
+      ></RadioGroup>
+
+      <div style={{ marginTop: 20 }}>
+        <RadioGroup
+          button="fill"
+          options={options1}
+          value={v}
+          onChange={(v) => {
+            console.log(v);
+            setV(v);
+          }}
+        ></RadioGroup>
+      </div>
     </StyledContent>
   );
 }
