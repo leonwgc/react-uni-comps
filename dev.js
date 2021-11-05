4bb0e16e4c99e8af2cf13797c8846bdd9df9a76f
leonwgc <giantfish@126.com>
2021-11-05 18:16:36 +0800

    chore : update dev

--- dev.js
+++ dev.js
@@ -1,6 +1,6 @@
 const { run } = require('vited');
 
-// modify index.html , use ./demo/index
+// modify index.html , use  <script type="module" src="./demo/index"></script>
 
 run(true, {
   server: {
