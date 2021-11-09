83cba2609620b69f8b9e403d0eb72a97926764e8
leonwgc <giantfish@126.com>
2021-11-09 12:56:47 +0800

    feat: 更新text

--- demo/Text.jsx
+++ demo/Text.jsx
@@ -1,33 +1,28 @@
-import React, { useState } from 'react';
-import { Text, Space, Tooltip } from '../src';
+import React from 'react';
+import { Text, Tooltip } from '../src';
+
+const text =
+  '规定了一个监听目标与边界盒交叉区域的比例值，可以是一个具体的数值或是一组0.0到1.0之间的数组。若指定值为0.0，则意味着监听元素即使与根有1像素交叉，此元素也会被视为可见.';
 
 export default function App() {
   return (
-    <div style={{ marginTop: 300, width: 400 }}>
+    <div style={{ margin: '20px', width: 400 }}>
       <div>
-        <Tooltip
-          title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere
-            mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo."
-        >
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere
-            mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+        <Tooltip title={text} arrow={false} placement="bottom-left">
+          <Text style={{ width: 200 }}>
+            <span style={{ color: 'red' }}>hello,world</span> <span>{text}</span>
           </Text>
         </Tooltip>
       </div>
 
       <div style={{ marginTop: 30 }}>
         <Tooltip
-          title={
-            <div style={{ width: 240, padding: 16 }}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere
-              mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-            </div>
-          }
+          arrow={false}
+          placement="bottom-left"
+          title={<div style={{ width: 240 }}>{text}</div>}
         >
-          <Text lines={2}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere
-            mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          <Text lines={2} style={{ width: 200 }}>
+            {text}
           </Text>
         </Tooltip>
       </div>
--- src/Text.tsx
+++ src/Text.tsx
@@ -1,14 +1,14 @@
 import React, { HTMLAttributes } from 'react';
 import styled from 'styled-components';
 
-const StyledSpanMultiLines = styled.span<{ lines?: number }>`
+const StyledMultiLines = styled.div<{ lines?: number }>`
   display: -webkit-box;
   -webkit-box-orient: vertical;
   -webkit-line-clamp: ${(props) => props.lines};
   overflow: hidden;
 `;
 
-const StyledSpanOneline = styled.span`
+const StyledLine = styled.div`
   display: block;
   overflow: hidden;
   text-overflow: ellipsis;
@@ -18,20 +18,18 @@ const StyledSpanOneline = styled.span`
 type Props = {
   /** 显示几行，超过显示省略号, 默认1 */
   lines?: number;
-  /** 包裹的文本 */
+  /** 包裹的内容 */
   children?: React.ReactNode;
-} & HTMLAttributes<HTMLSpanElement>;
+  style?: React.CSSProperties;
+  className?: string;
+} & HTMLAttributes<HTMLDivElement>;
 
 /** 文本显示，1.超过行数显示省略号 2.单行超过宽度显示省略号 */
 const Text = React.forwardRef<HTMLSpanElement, Props>((props: Props, ref) => {
   const { lines = 1, children, ...rest } = props;
 
-  if (typeof children !== 'string' || typeof lines !== 'number') {
-    return <span ref={ref}>{children}</span>;
-  }
-
   return React.createElement(
-    lines > 1 ? StyledSpanMultiLines : StyledSpanOneline,
+    lines > 1 ? StyledMultiLines : StyledLine,
     {
       ...rest,
       ref,
