478cabc97c8b264b4b68a798f0df18e20b4f2f90
leonwgc <giantfish@126.com>
2021-11-09 15:43:54 +0800

    chore :update demo

--- demo/Drawer.jsx
+++ demo/Drawer.jsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useState } from 'react';
-import { Icon, Tabs, Space, Button, Avatar, Drawer, isMobile } from '../src';
+import { Tabs, Space, Button, Avatar, Drawer } from '../src';
 import styled from '../src/styled';
 
 // example to implement Drawer
@@ -21,19 +21,6 @@ const StyledTabs = styled(Tabs)`
   }
 `;
 
-const StyledPopupContent = styled.div`
-  width: 640px;
-  background: #ffffff;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-
-const StyledBody = styled.div`
-  padding: 20px;
-  flex: 1;
-`;
-
 const StyledFooter = styled.div`
   flex-basis: 50px;
   height: 50px;
@@ -44,51 +31,11 @@ const StyledFooter = styled.div`
   padding: 0 20px;
 `;
 
-const StyledRightGroup = styled.div`
-  .btn-new {
-    width: 124px;
-    height: 36px;
-    border: 1px solid #e6e6e6;
-    border-radius: 4px;
-  }
-  .card-list {
-    .card-item {
-      display: flex;
-      height: 65px;
-      background: #ffffff;
-      border: 1px solid #ebebeb;
-      border-radius: 4px;
-      margin: 8px 0;
-      padding: 18px 12px;
-      align-items: center;
-
-      .content {
-        .title {
-          font-size: 14px;
-          font-family: PingFangSC, PingFangSC-Regular;
-          font-weight: 400;
-          text-align: left;
-          color: #1a1a1a;
-          line-height: 20px;
-        }
-        .desc {
-          font-size: 12px;
-          font-family: PingFangSC, PingFangSC-Regular;
-          font-weight: 400;
-          text-align: left;
-          color: #8c8c8c;
-          line-height: 17px;
-        }
-      }
-    }
-  }
+const StyledBody = styled.div`
+  padding: 16px;
 `;
 
 export default function RightDrawer(props) {
-  const [list, setList] = useState([
-    { title: '默认', desc: '公司内拥有OKR权限且一级部门相同，拥有汇报线关联关系的用户' },
-    { title: '仅上级可见', desc: '可用于需保密的 OKR 上' },
-  ]);
   const [index, setIndex] = useState(0);
   const [visible, setVisible] = useState(false);
 
@@ -106,17 +53,17 @@ export default function RightDrawer(props) {
         position="right"
         onClose={onClose}
         visible={visible}
-        wrapStyle={{ width: isMobile ? '80vw' : 600 }}
+        wrapStyle={{ width: '60vw' }}
         header={
           <StyledTabs underline={false} value={index} onChange={setIndex}>
-            <Tabs.Tab title="权限设置" />
-            <Tabs.Tab title="权限组管理" />
+            <Tabs.Tab title="tab1" />
+            <Tabs.Tab title="tab2" />
           </StyledTabs>
         }
         footer={
           <StyledFooter>
             <Space size={16}>
-              <Button type="primary" style={{ width: 80 }}>
+              <Button type="primary" style={{ width: 80 }} onClick={onClose}>
                 保存
               </Button>
               <Button style={{ width: 80 }} onClick={onClose}>
@@ -126,23 +73,12 @@ export default function RightDrawer(props) {
           </StyledFooter>
         }
       >
-        <StyledRightGroup style={{ padding: 20 }}>
-          <Button className="btn-new" icon={<Icon type="uc-icon-jia2" />}>
-            新建权限组
-          </Button>
-          <div className="card-list">
-            {list.map((item, idx) => (
-              <div className="card-item" key={idx}>
-                <Avatar size={28} style={{ marginRight: 12, flex: 'none' }}></Avatar>
-                <div className="content">
-                  <div className="title">{item?.title}</div>
-                  <div className="desc">{item?.desc}</div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </StyledRightGroup>
+        <StyledBody>
+          <Avatar /> {index === 0 ? 'tab1 body' : 'tab2 body'}
+        </StyledBody>
       </Drawer>
+
+     
     </>
   );
 }
