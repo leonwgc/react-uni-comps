abc71a2a3c5bfd23a380218be8a303d44ac7514c
leonwgc <giantfish@126.com>
2021-11-21 10:28:28 +0800

    chore: 1.0.6 update

--- demo/Spinner.jsx
+++ demo/Spinner.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Spinner, Space } from '../src';
-import { CSSTransition, Transition, TransitionGroup } from '../src/transition';
 
 export default function App() {
   return (
@@ -8,7 +7,6 @@ export default function App() {
       <Space wrap>
         <Spinner style={{ color: 'red', fontSize: 30, margin: '0 20px' }}></Spinner>
         <Spinner size={32}></Spinner>
-        <Spinner></Spinner>
         <Spinner></Spinner>
         <Spinner color="red" size={48}></Spinner>
         <Spinner color="#004bcc"></Spinner>
