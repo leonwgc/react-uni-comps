2122994d4b16fd74f5535868e37dcc800351d3c9
leonwgc <giantfish@126.com>
2021-11-27 16:13:47 +0800

    chore: remove unused

--- src/vars.ts
+++ src/vars.ts
@@ -1,9 +1,7 @@
 /** 统一浅色边框色 */
 export const border = '#eee';
-export const shadow = `0 3px 6px -4px rgba(0, 0, 0, 0.12), 0 6px 16px 0 rgba(0, 0, 0, 0.08),
-0 9px 28px 8px rgba(0, 0, 0, 0.05)`;
 
-export const disabledText = 'rgba(0, 0, 0, 0.25)'; // text
+export const disabledText = 'rgba(0, 0, 0, 0.25)';
 export const disabledBg = '#f5f5f5';
 export const primary = '#004bcc';
 export const danger = '#ff4d4f';
