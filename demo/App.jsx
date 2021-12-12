e28c5ec002b57ee4c266ee347d5d35abff62b099
leonwgc <giantfish@126.com>
2021-12-12 16:00:27 +0800

    feat: change theme via js

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,22 +1,70 @@
 import React, { Suspense } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { ThemeProvider } from 'react-uni-comps';
+import { ThemeProvider, styled, PopMenu, Space } from 'react-uni-comps';
+import { useSelector, useUpdateStore } from 'simple-redux-store';
 import routes from './RouteConfig';
 
+const StyledSetting = styled.div`
+  position: fixed;
+  right: 8px;
+  top: 8px;
+  width: 20px;
+  height: 20px;
+  border: 4px solid #fff;
+  box-shadow: 0 1px 2px 0 rgba(56, 56, 56, 0.15);
+  cursor: pointer;
+`;
+
+const StyledPopMenu = styled(PopMenu)`
+  .list {
+    padding: 8px;
+    width: 180px;
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
 const Routes = () => {
+  const { theme } = useSelector((s) => s.app);
+  const updateStore = useUpdateStore();
   return (
-    <ThemeProvider color="#FF5D0D">
-      <Router history={history}>
-        <Suspense fallback={null}>
-          <Switch>
-            {routes.map((route, idx) => (
-              <Route key={idx} path={route.path} exact={route.exact} component={route.component} />
+    <div style={{ paddingTop: 32 }}>
+      <ThemeProvider color={theme}>
+        <Router history={history}>
+          <Suspense fallback={null}>
+            <Switch>
+              {routes.map((route, idx) => (
+                <Route
+                  key={idx}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.component}
+                />
+              ))}
+              <Route render={() => <div>page not found</div>} />
+            </Switch>
+          </Suspense>
+        </Router>
+      </ThemeProvider>
+      <StyledPopMenu
+        trigger="click"
+        content={
+          <div className="list">
+            {['green', 'blue', 'orange', 'purple', 'red'].map((i) => (
+              <div
+                style={{ background: i, width: 24, height: 24 }}
+                key={i}
+                onClick={() => {
+                  updateStore({ theme: i });
+                }}
+              ></div>
             ))}
-            <Route render={() => <div>page not found</div>} />
-          </Switch>
-        </Suspense>
-      </Router>
-    </ThemeProvider>
+          </div>
+        }
+      >
+        <StyledSetting style={{ background: theme }} />
+      </StyledPopMenu>
+    </div>
   );
 };
 
