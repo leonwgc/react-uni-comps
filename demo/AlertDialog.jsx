df56c4ab6240b9c129fcf2b91a64338587c1d83d
leonwgc <giantfish@126.com>
2021-12-18 10:25:56 +0800

    docs: update

--- demo/AlertDialog.jsx
+++ demo/AlertDialog.jsx
@@ -1,50 +1,54 @@
 import React from 'react';
-import { AlertDialog, Button, Toast, Divider, Icon, Space } from 'react-uni-comps';
+import StyledF12Tip from './common/StyledF12Tip';
+import { AlertDialog, Button, Toast, Divider, Icon, Space, ThemeProvider } from 'react-uni-comps';
 
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <Divider textPosition="left">只包含确定按钮, 代替window.alert</Divider>
-      <Button
-        onClick={() =>
-          AlertDialog.show('提示', '确定要这么做吗', '决定了', () => Toast.show('你点击了确定'))
-        }
-      >
-        模拟window.alert
-      </Button>
+    <ThemeProvider color="#FF5D0D">
+      <div style={{ padding: 20 }}>
+        <StyledF12Tip />
+        <Divider textPosition="left">只包含确定按钮, 代替window.alert</Divider>
+        <Button
+          onClick={() =>
+            AlertDialog.show('提示', '确定要这么做吗', '决定了', () => Toast.show('你点击了确定'))
+          }
+        >
+          模拟window.alert
+        </Button>
 
-      <Divider textPosition="left">包含确认和取消按钮,代替window.confirm</Divider>
-      <Button
-        onClick={() =>
-          AlertDialog.show(
-            '提示',
-            '确定要这么做吗',
-            '决定了',
-            () => Toast.show('你点击了确定'),
-            '取消',
-            () => Toast.show('你点击了取消')
-          )
-        }
-      >
-        模拟window.confirm
-      </Button>
+        <Divider textPosition="left">包含确认和取消按钮,代替window.confirm</Divider>
+        <Button
+          onClick={() =>
+            AlertDialog.show(
+              '提示',
+              '确定要这么做吗',
+              '决定了',
+              () => Toast.show('你点击了确定'),
+              '取消',
+              () => Toast.show('你点击了取消')
+            )
+          }
+        >
+          模拟window.confirm
+        </Button>
 
-      <Divider textPosition="left">自定义样式</Divider>
-      <Button
-        onClick={() =>
-          AlertDialog.show(
-            '提示',
-            <Space style={{ textAlign: 'center', color: 'red', fontSize: 18 }}>
-              <Icon type="uc-icon-jinggao" />
-              确定要这么做吗
-            </Space>,
-            '决定了',
-            () => Toast.show('你点击了确定')
-          )
-        }
-      >
-        模拟window.alert
-      </Button>
-    </div>
+        <Divider textPosition="left">自定义样式</Divider>
+        <Button
+          onClick={() =>
+            AlertDialog.show(
+              '提示',
+              <Space style={{ textAlign: 'center', color: 'red', fontSize: 18 }}>
+                <Icon type="uc-icon-jinggao" />
+                确定要这么做吗
+              </Space>,
+              '决定了',
+              () => Toast.show('你点击了确定')
+            )
+          }
+        >
+          模拟window.alert
+        </Button>
+      </div>
+    </ThemeProvider>
   );
 }
---
+++ demo/common/StyledF12Tip.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { styled } from 'react-uni-comps';
+
+const StyledTip = styled.div`
+  font-size: 20px;
+  margin: 24px 0;
+`;
+
+export default function StyledF12Tip() {
+  return <StyledTip>F12预览移动端效果</StyledTip>;
+}
