36df2b2c3017a188a36aa0caaaa3cbb0f43ca591
leonwgc <giantfish@126.com>
2022-01-13 14:00:10 +0800

    chore: update demo

--- demo/QRCode.jsx
+++ demo/QRCode.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import PageWrap from './common/PageWrap';
 import DemoBlock from './common/Block';
 import { QRCode } from 'react-uni-comps';
@@ -7,23 +7,22 @@ export default function App() {
   const [v, setV] = useState(0);
   const text = v == 0 ? 'https://www.baidu.com/' : 'https://juejin.cn/';
 
+  const changeText = useCallback(() => {
+    setV((v) => (v == 0 ? 1 : 0));
+  }, []);
+
   return (
     <PageWrap>
       <DemoBlock title="默认">
-        <QRCode text={text} onClick={() => setV((v) => (v == 0 ? 1 : 0))}></QRCode>
+        <QRCode text={text} onClick={changeText}></QRCode>
       </DemoBlock>
 
       <DemoBlock title="尺寸">
-        <QRCode text={text} size={180} onClick={() => setV((v) => (v == 0 ? 1 : 0))}></QRCode>
+        <QRCode text={text} size={180} onClick={changeText}></QRCode>
       </DemoBlock>
 
       <DemoBlock title="颜色">
-        <QRCode
-          text={text}
-          colorDark="red"
-          colorLight="#ccc"
-          onClick={() => setV((v) => (v == 0 ? 1 : 0))}
-        ></QRCode>
+        <QRCode text={text} colorDark="red" colorLight="#ccc" onClick={changeText}></QRCode>
       </DemoBlock>
     </PageWrap>
   );
