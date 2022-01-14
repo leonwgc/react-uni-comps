d96878013337cfbacc3ce5fb244cd1f0a1ad3109
leonwgc <giantfish@126.com>
2022-01-14 17:53:46 +0800

    chore: update 防闪烁 demo

--- demo/WaitLoading.jsx
+++ demo/WaitLoading.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from 'react';
+import PageWrap from './common/PageWrap';
+import DemoBlock from './common/Block';
 import { Button, Space, WaitLoading, Spin } from 'react-uni-comps';
 
 export default function App() {
   const [v, setV] = useState(false);
   return (
-    <div style={{ margin: 20 }}>
-      <Space>
-        <Button type="primary" onClick={() => setV(true)} loading={v}>
-          1秒后显示 loading
-        </Button>
+    <PageWrap>
+      <DemoBlock title="防闪烁">
+        <Space>
+          <Button type="primary" onClick={() => setV(true)} loading={v}>
+            1秒后显示 loading
+          </Button>
 
-        <Button onClick={() => setV(false)}>隐藏loading </Button>
-      </Space>
+          <Button onClick={() => setV(false)}>隐藏loading </Button>
+        </Space>
 
-      <div style={{ marginTop: 24 }}>
-        <WaitLoading wait={1000} visible={v}>
-          <Spin style={{ fontSize: 30, color: '#00bc8d' }} />
-        </WaitLoading>
-      </div>
-    </div>
+        <div style={{ marginTop: 24 }}>
+          <WaitLoading wait={1000} visible={v}>
+            <Spin style={{ fontSize: 30, color: '#00bc8d' }} />
+          </WaitLoading>
+        </div>
+      </DemoBlock>
+    </PageWrap>
   );
 }
