424f33cd202da51f48bdb9cbfe5982183d04850f
leonwgc <giantfish@126.com>
2022-01-15 09:12:05 +0800

    chore: remove copy-text-to-clipboard

--- src/CopyToClipboard.tsx
+++ src/CopyToClipboard.tsx
@@ -1,5 +1,55 @@
 import React from 'react';
-import copy from 'copy-text-to-clipboard';
+
+/**
+ * https://github.com/sindresorhus/copy-text-to-clipboard
+ *
+ * @param {*} input
+ * @param {*} [{ target = document.body }={}]
+ * @return {*}
+ */
+function copy(input, { target = document.body } = {}) {
+  const element = document.createElement('textarea');
+  const previouslyFocusedElement: HTMLElement = document.activeElement as HTMLElement;
+
+  element.value = input;
+
+  // Prevent keyboard from showing on mobile
+  element.setAttribute('readonly', '');
+
+  element.style.contain = 'strict';
+  element.style.position = 'absolute';
+  element.style.left = '-9999px';
+  element.style.fontSize = '12pt'; // Prevent zooming on iOS
+
+  const selection = document.getSelection();
+  const originalRange = selection.rangeCount > 0 && selection.getRangeAt(0);
+
+  target.append(element);
+  element.select();
+
+  // Explicit selection workaround for iOS
+  element.selectionStart = 0;
+  element.selectionEnd = input.length;
+
+  let isSuccess = false;
+  try {
+    isSuccess = document.execCommand('copy');
+  } catch {}
+
+  element.remove();
+
+  if (originalRange) {
+    selection.removeAllRanges();
+    selection.addRange(originalRange);
+  }
+
+  // Get the focus back on the previously focused element, if any
+  if (previouslyFocusedElement) {
+    previouslyFocusedElement.focus();
+  }
+
+  return isSuccess;
+}
 
 type Props = {
   /** 要复制的文本 */
@@ -9,7 +59,8 @@ type Props = {
   /** 包裹的元素,只能一个 */
   children: React.ReactElement;
 };
-/** 复制文本到剪贴板 */
+
+/** 复制文本*/
 const CopyToClipboard = React.forwardRef<React.ReactElement, Props>(
   (props: Props, ref): React.ReactElement => {
     const { text, onCopy, children } = props;
