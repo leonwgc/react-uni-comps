81bcc0d14e2030d7f45f0e872c119b76ba47ce71
leonwgc <giantfish@126.com>
2022-02-12 11:38:13 +0800

    chore: update demo

--- demo/FingerGestureElement.tsx
+++ demo/FingerGestureElement.tsx
@@ -31,56 +31,56 @@ export default function App() {
   return (
     <PageWrap>
       <DemoBlock title="当前值：" style={{ background: '#fff' }}>
-        <AutoCenter>
-          <div ref={statusElRef}></div>
-        </AutoCenter>
+        <div ref={statusElRef}></div>
       </DemoBlock>
 
       <DemoBlock title="基础分割线">
-        <FingerGestureElement
-          onDoubleTap={() => {
-            Toast.show('doubleTap');
-          }}
-          onLongTap={() => {
-            Toast.show('longTap');
-          }}
-          onTouchStart={() => {
-            console.log('start');
-          }}
-          onTouchEnd={(e) => {
-            console.log('end');
-          }}
-          onSwipe={(e) => {
-            console.log('swipe', e.direction);
-          }}
-          onPinch={(e) => {
-            console.log('scale:', e.scale);
-            ref.current.scale = e.scale;
-            update(elRef.current, ref.current, statusElRef.current);
-          }}
-          onRotate={(e) => {
-            console.log('angle:', e.angle);
-            ref.current.angle += e.angle;
-            update(elRef.current, ref.current, statusElRef.current);
-          }}
-          onPressMove={(e) => {
-            console.log(e.deltaX, e.deltaY);
-            ref.current.x = ref.current.x + e.deltaX;
-            ref.current.y = ref.current.y + e.deltaY;
-
-            update(elRef.current, ref.current, statusElRef.current);
-          }}
-          ref={elRef}
-        >
-          <div
-            style={{
-              width: 100,
-              height: 100,
-              backgroundColor: 'red',
-              position: 'relative',
+        <AutoCenter>
+          <FingerGestureElement
+            onDoubleTap={() => {
+              Toast.show('doubleTap');
             }}
-          />
-        </FingerGestureElement>
+            onLongTap={() => {
+              Toast.show('longTap');
+            }}
+            onTouchStart={() => {
+              console.log('start');
+            }}
+            onTouchEnd={(e) => {
+              console.log('end');
+            }}
+            onSwipe={(e) => {
+              console.log('swipe', e.direction);
+            }}
+            onPinch={(e) => {
+              console.log('scale:', e.scale);
+              ref.current.scale = e.scale;
+              update(elRef.current, ref.current, statusElRef.current);
+            }}
+            onRotate={(e) => {
+              console.log('angle:', e.angle);
+              ref.current.angle += e.angle;
+              update(elRef.current, ref.current, statusElRef.current);
+            }}
+            onPressMove={(e) => {
+              console.log(e.deltaX, e.deltaY);
+              ref.current.x = ref.current.x + e.deltaX;
+              ref.current.y = ref.current.y + e.deltaY;
+
+              update(elRef.current, ref.current, statusElRef.current);
+            }}
+            ref={elRef}
+          >
+            <div
+              style={{
+                width: 120,
+                height: 120,
+                border: '1px solid red',
+                position: 'relative',
+              }}
+            />
+          </FingerGestureElement>
+        </AutoCenter>
       </DemoBlock>
     </PageWrap>
   );
