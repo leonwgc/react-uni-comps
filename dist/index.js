a60f7df3d6f20e8873f7d1312c2449b2df4639b7
leonwgc <giantfish@126.com>
2022-03-13 11:14:21 +0800

    chore: update lib

--- dist/index.js
+++ dist/index.js
@@ -2100,7 +2100,7 @@ var Tabs$1 = attachPropertiesToComponent(Tabs, {
 var _excluded$8 = ["title", "required", "label", "labelWidth", "description", "className", "content", "lineColor", "children"];
 
 var _templateObject$9;
-var StyledCell = styled__default['default'].div(_templateObject$9 || (_templateObject$9 = _taggedTemplateLiteral(["\n  background-color: #fff;\n\n  &.clickable {\n    &:active {\n      background-color: ", ";\n    }\n  }\n\n  &.has-label {\n    padding-left: 12px;\n  }\n\n  .cell-inner {\n    position: relative;\n    display: flex;\n    box-sizing: border-box;\n    width: 100%;\n    padding: 10px 12px 10px 0;\n    overflow: hidden;\n    font-size: 14px;\n    line-height: 24px;\n\n    &.pc {\n      align-items: center;\n    }\n\n    .cell-label {\n      box-sizing: border-box;\n      margin-right: 12px;\n      text-align: left;\n      flex: 1;\n\n      .label {\n        color: #333;\n\n        &.required::before {\n          content: '*';\n          margin-right: 2px;\n          color: ", ";\n          vertical-align: middle;\n        }\n      }\n\n      .description {\n        color: #999;\n        margin-top: 4px;\n        line-height: 18px;\n        font-size: 12px;\n      }\n\n      &.input {\n        word-wrap: break-word;\n        width: 6.2em;\n        flex: none;\n      }\n    }\n    .cell-content {\n      flex: 1;\n      position: relative;\n      overflow: visible;\n      color: #999;\n      text-align: right;\n      vertical-align: middle;\n      word-wrap: break-word;\n\n      &.input {\n        display: flex;\n        align-items: center;\n      }\n    }\n  }\n"])), activeBg, danger);
+var StyledCell = styled__default['default'].div(_templateObject$9 || (_templateObject$9 = _taggedTemplateLiteral(["\n  background-color: #fff;\n\n  &.clickable {\n    &:active {\n      background-color: ", ";\n    }\n  }\n\n  &.has-label {\n    padding-left: 12px;\n  }\n\n  .cell-inner {\n    position: relative;\n    display: flex;\n    box-sizing: border-box;\n    width: 100%;\n    padding: 10px 12px 10px 0;\n    overflow: hidden;\n    font-size: 14px;\n    line-height: 24px;\n\n    &.pc {\n      align-items: center;\n    }\n\n    .cell-label {\n      box-sizing: border-box;\n      text-align: left;\n      flex: 1;\n\n      .label {\n        color: #333;\n\n        &.required::before {\n          content: '*';\n          margin-right: 2px;\n          color: ", ";\n          vertical-align: middle;\n        }\n      }\n\n      .description {\n        color: #999;\n        margin-top: 4px;\n        line-height: 18px;\n        font-size: 12px;\n      }\n\n      &.input {\n        word-wrap: break-word;\n        width: 6.2em;\n        flex: none;\n      }\n    }\n    .cell-content {\n      flex: 1;\n      position: relative;\n      overflow: visible;\n      color: #999;\n      text-align: right;\n      vertical-align: middle;\n      word-wrap: break-word;\n\n      &.input {\n        display: flex;\n        align-items: center;\n      }\n    }\n  }\n"])), activeBg, danger);
 /** 列表项，通常用于移动端 */
 
 var Cell = /*#__PURE__*/React__default['default'].forwardRef(function (props, ref) {
@@ -2122,6 +2122,7 @@ var Cell = /*#__PURE__*/React__default['default'].forwardRef(function (props, re
 
   var hasInput = !!children;
   var hasLabel = label || title;
+  var hasContent = content || children;
   return /*#__PURE__*/React__default['default'].createElement(StyledCell, _extends({}, rest, {
     ref: ref,
     className: clsx__default['default']('uc-cell', className, {
@@ -2149,7 +2150,7 @@ var Cell = /*#__PURE__*/React__default['default'].forwardRef(function (props, re
     })
   }, label || title), description && /*#__PURE__*/React__default['default'].createElement("div", {
     className: "description"
-  }, description)), /*#__PURE__*/React__default['default'].createElement("div", {
+  }, description)), hasContent && /*#__PURE__*/React__default['default'].createElement("div", {
     className: clsx__default['default']('cell-content', {
       input: hasInput
     })
