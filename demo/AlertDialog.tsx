cda97ed6510839eacbcb115596e5ae3bbe32f33d
leonwgc <giantfish@126.com>
2022-04-21 16:44:34 +0800

    chore: AlertDialog

--- demo/AlertDialog.tsx
+++ demo/AlertDialog.tsx
@@ -1,42 +1,60 @@
 import React from 'react';
 import PageWrap from './common/PageWrap';
 import DemoBlock from './common/DemoBlock';
-import { AlertDialog, Button, Toast } from 'react-uni-comps';
+import { AlertDialog, Button, Toast, Space } from 'react-uni-comps';
 
 export default function App() {
   return (
     <PageWrap>
       <DemoBlock title="只包含确定按钮, 代替window.alert">
-        <Button
-          onClick={() =>
-            AlertDialog.show({
-              title: '提示',
-              content: '确定要这么做吗',
-              confirmText: '决定了',
-              onConfirm: (close) => {
-                close();
-              },
-            })
-          }
-        >
-          模拟window.alert
-        </Button>
+        <Space>
+          <Button
+            onClick={() =>
+              AlertDialog.show({
+                title: '提示',
+                content: '确定要这么做吗',
+                confirmText: '决定了',
+                onConfirm: (close) => {
+                  close();
+                },
+              })
+            }
+          >
+            模拟window.alert
+          </Button>
 
-        <Button
-          onClick={() =>
-            AlertDialog.show({
-              title: '提示',
-              content: '确定要这么做吗',
-              confirmText: null,
-              cancelText: '决定了',
-              onCancel: (close) => {
-                close();
-              },
-            })
-          }
-        >
-          取消
-        </Button>
+          <Button
+            onClick={() =>
+              AlertDialog.show({
+                title: '提示',
+                content: '确定要这么做吗',
+                confirmText: null,
+                cancelText: '决定了',
+                onCancel: (close) => {
+                  close();
+                },
+              })
+            }
+          >
+            取消
+          </Button>
+
+          <Button
+            onClick={() =>
+              AlertDialog.show({
+                closable: true,
+                title: '提示',
+                content: '确定要这么做吗',
+                confirmText: '决定了',
+                onConfirm: (close) => {
+                  close();
+                },
+              })
+            }
+          >
+            显示关闭
+          </Button>
+        </Space>
       </DemoBlock>
 
       <DemoBlock title="包含确认和取消按钮,代替window.confirm">
