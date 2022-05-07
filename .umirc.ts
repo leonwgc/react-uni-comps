b709ad8a01e6dea0d73d1bf818708516839d1c74
leonwgc <giantfish@126.com>
2022-05-07 10:56:28 +0800

    feat: split online demo codes

--- .umirc.ts
+++ .umirc.ts
@@ -6,7 +6,7 @@ const logo =
   'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K';
 
 export default defineConfig({
-  title: repo,
+  title: 'react-uni-comps',
   favicon: logo,
   logo: logo,
   outputPath: 'docs',
@@ -19,13 +19,14 @@ export default defineConfig({
       output: './.mfsu-dev',
     },
   },
+  dynamicImport: {},
   locales: [['zh-CN', '中文']],
   hash: true,
   webpack5: {},
   fastRefresh: {},
   // Because of using GitHub Pages
-  base: `/${repo}/`,
-  publicPath: `/${repo}/`,
+  base: repo ? `/${repo}/` : '/',
+  publicPath: repo ? `/${repo}/` : '/',
   styles: [
     `
     #root .__dumi-default-menu-header p {
