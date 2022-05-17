ec77b96c0eedd3b7d7110b5108253920418b7cd7
leonwgc <giantfish@126.com>
2022-05-17 20:12:11 +0800

    chore: drawer demo

--- demo/Drawer.tsx
+++ demo/Drawer.tsx
@@ -1,33 +1,18 @@
 import React, { useCallback, useState } from 'react';
 import PageWrap from './common/PageWrap';
 import DemoBlock from './common/DemoBlock';
-import { Tabs, Space, Button, Drawer, styled, isMobile } from 'react-uni-comps';
-
-const StyledTabs = styled(Tabs)`
-  .uc-tabs-header-wrap {
-    height: 50px;
-    padding-left: 20px;
-  }
-  .uc-tabs-header-item {
-    width: 120px;
-
-    &.active {
-      background: rgba(0, 75, 204, 0.08);
-    }
-  }
-`;
+import { Space, Button, Drawer, styled, isMobile } from 'react-uni-comps';
 
 const StyledFooter = styled.div`
   height: 50px;
   background: #ffffff;
   box-shadow: 0px -1px 0px 0px #f5f5f5;
   display: flex;
-  align-items: center;
-  padding-left: 20px;
+  justify-content: flex-end;
+  padding: 0 20px;
 `;
 
 export default function RightDrawer(props) {
-  const [index, setIndex] = useState(0);
   const [visible, setVisible] = useState(false);
 
   const onClose = useCallback(() => {
@@ -37,22 +22,14 @@ export default function RightDrawer(props) {
   return (
     <PageWrap>
       <DemoBlock title="抽屉">
-        <Button onClick={() => setVisible(true)} style={{ margin: 20 }}>
-          打开抽屉
-        </Button>
+        <Button onClick={() => setVisible(true)}>打开抽屉</Button>
       </DemoBlock>
 
       <Drawer
-        position="right"
         onClose={onClose}
         visible={visible}
         style={{ width: isMobile ? '80vw' : 420 }}
-        header={
-          <StyledTabs underline={false} value={index} onChange={(i) => setIndex(i)}>
-            <Tabs.Tab title="tab1" />
-            <Tabs.Tab title="tab2" />
-          </StyledTabs>
-        }
+        header={<div style={{ padding: 20, fontSize: 20 }}>标题</div>}
         footer={
           <StyledFooter>
             <Space size={16}>
@@ -66,7 +43,7 @@ export default function RightDrawer(props) {
           </StyledFooter>
         }
       >
-        <div style={{ padding: 20 }}>{index === 0 ? 'tab1 ' : 'tab2 '}</div>
+        <div style={{ padding: 20 }}>this is content</div>
       </Drawer>
     </PageWrap>
   );
