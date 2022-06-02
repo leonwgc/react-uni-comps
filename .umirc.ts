75aa3d4dc1e6752942ffaeabb7aae8a5f5a4dcd6
leonwgc <giantfish@126.com>
2022-06-02 21:03:52 +0800

    doc: dumi config

--- .umirc.ts
+++ .umirc.ts
@@ -26,13 +26,27 @@ export default defineConfig({
   fastRefresh: {},
   apiParser: {
     // 自定义属性过滤配置，也可以是一个函数，用法参考：https://github.com/styleguidist/react-docgen-typescript/#propfilter
-    propFilter: {
-      // 是否忽略从 node_modules 继承的属性，默认值为 false
-      skipNodeModules: true,
-      // 需要忽略的属性名列表，默认为空数组
-      skipPropsWithName: [],
-      // 是否忽略没有文档说明的属性，默认值为 false
-      skipPropsWithoutDoc: false,
+    // propFilter: {
+    //   // 是否忽略从 node_modules 继承的属性，默认值为 false
+    //   skipNodeModules: true,
+    //   // 需要忽略的属性名列表，默认为空数组
+    //   skipPropsWithName: [],
+    //   // 是否忽略没有文档说明的属性，默认值为 false
+    //   skipPropsWithoutDoc: false,
+    // },
+    propFilter: (prop, component) => {
+      if (prop.declarations !== undefined && prop.declarations.length > 0) {
+        const hasPropAdditionalDescription = prop.declarations.find((declaration) => {
+          return (
+            !declaration.fileName.includes('node_modules') ||
+            declaration.fileName.includes('Popover')
+          );
+        });
+
+        return Boolean(hasPropAdditionalDescription);
+      }
+
+      return true;
     },
   },
   // Because of using GitHub Pages
