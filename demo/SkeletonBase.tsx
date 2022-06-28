6ecbe9eaf5fbbd6348e69037aeed3ba62f986e7c
leonwgc <giantfish@126.com>
2022-06-28 17:47:54 +0800

    chore: SkeletonBase demo

--- demo/SkeletonBase.tsx
+++ demo/SkeletonBase.tsx
@@ -13,6 +13,10 @@ export default function App() {
       <DemoBlock title="圆形">
         <SkeletonBase shape="circle" style={{ width: 60, height: 60 }} />
       </DemoBlock>
+
+      <DemoBlock title="颜色">
+        <SkeletonBase style={{ height: 10, background: '#00bc8d' }} />
+      </DemoBlock>
     </PageWrap>
   );
 }
