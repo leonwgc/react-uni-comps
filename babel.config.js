fd1a7fb7aa16a0ecb43a1d72053e026ce305010d
leonwgc <giantfish@126.com>
2022-07-07 21:02:46 +0800

    chore: update deps

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,8 @@
-// module.exports = {
-//   presets: [
-//     require.resolve('@babel/preset-env'),
-//     require.resolve('@babel/preset-react'),
-//     require.resolve('@babel/preset-typescript'),
-//   ],
-//   plugins: [
-//     ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
-//     ['@babel/plugin-proposal-private-methods', { loose: false }],
-//     ['@babel/plugin-proposal-class-properties', { loose: false }],
-//   ],
-// };
-
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    require.resolve('@babel/preset-env'),
     require.resolve('@babel/preset-react'),
-    '@babel/preset-typescript',
+    require.resolve('@babel/preset-typescript'),
   ],
   plugins: [
     [
