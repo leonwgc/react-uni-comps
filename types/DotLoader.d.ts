6c5e6be9053d08db1be79a75231891b433a661c7
leonwgc <giantfish@126.com>
2022-07-07 21:07:17 +0800

    chore: tsconfig update

--- types/DotLoader.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-declare type Props = React.HTMLAttributes<HTMLDivElement>;
-/** 加载中 */
-declare const Spin: React.ForwardRefExoticComponent<Props & React.RefAttributes<HTMLDivElement>>;
-export default Spin;
--- types/popovers/types.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type SinglePlacement = 'top' | 'bottom' | 'left' | 'right';
-export declare type Placement = 'top' | 'left' | 'bottom' | 'right' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right' | 'left-top' | 'left-bottom' | 'right-top' | 'right-bottom' | SinglePlacement;
-export interface ITargetPos {
-    left: number;
-    top: number;
-    width: number;
-    height: number;
-}
