a3fb7227e5432271c50e4ba41d73232115e74042
leonwgc <giantfish@126.com>
2022-07-25 16:21:16 +0800

    fix: babel for jest

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  presets: [
+    require.resolve('@babel/preset-env'),
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-typescript'),
+  ],
   plugins: [
     [
       'babel-plugin-styled-components',
