82cf87b01c0ca9e76c95463b8c160d4af9088beb
leonwgc <giantfish@126.com>
2022-07-25 16:02:01 +0800

    refactor: mount & unmount hooks

--- src/hooks/useMount.tsx
+++ src/hooks/useMount.tsx
@@ -1,18 +1,12 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import { useLayoutEffect, useRef } from 'react';
+import { useEffect } from 'react';
 /**
  *  组件加载执行回调
  *
  * @param {() => void} fn 加载执行的回调
  */
-const useMount = (fn: () => void): void => {
-  const isMounted = useRef(false);
-
-  useLayoutEffect(() => {
-    if (!isMounted.current) {
-      isMounted.current = true;
-      fn?.();
-    }
+const useMount = (fn: () => void) => {
+  useEffect(() => {
+    fn?.();
   }, []);
 };
 
--- src/hooks/useUnmount.tsx
+++ src/hooks/useUnmount.tsx
@@ -1,16 +1,15 @@
-import { useLayoutEffect } from 'react';
+import { useEffect } from 'react';
 
 /**
  *  组件卸载执行回调
  *
  * @param {() => void} fn 执行的回调
  */
-const useUnmount = (fn: () => void): void => {
-  useLayoutEffect(() => {
+const useUnmount = (fn: () => void) => {
+  useEffect(() => {
     return () => {
       fn?.();
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
 
