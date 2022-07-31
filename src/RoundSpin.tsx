a5ec939b269b6fbdcb7935fe16edf0a538bc27ef
leonwgc <giantfish@126.com>
2022-07-31 15:46:54 +0800

    fix: round spin in wechat

--- src/RoundSpin.tsx
+++ src/RoundSpin.tsx
@@ -44,13 +44,17 @@ const move = keyframes`
     }
 `;
 
+const SVGProps = {
+  width: '1em',
+  height: '1em',
+  fill: 'none',
+};
+
 const StyledLoader = styled.div<{ $duration: number }>`
   display: inline-flex;
   vertical-align: middle;
   transform: rotate(-90deg);
   animation: ${rotate} 2s linear infinite;
-  width: 1em;
-  height: 1em;
 
   svg {
     animation: ${move} ${({ $duration }) => $duration}ms ease-in-out infinite;
@@ -71,13 +75,12 @@ const RoundSpin = React.forwardRef<HTMLDivElement, Props>((props, ref) => {
       $duration={duration}
       className={clsx(className, 'uc-round-spin')}
     >
-      <svg viewBox={`${radius} ${radius} ${radius * 2} ${radius * 2}`}>
+      <svg {...SVGProps} viewBox={`${radius} ${radius} ${radius * 2} ${radius * 2}`}>
         <circle
           cx={radius * 2}
           cy={radius * 2}
           r={radius - (strokeWidth as number) / 2}
           stroke={color}
-          fill="none"
           strokeLinecap="round"
           style={{ strokeWidth }}
         />
