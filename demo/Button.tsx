497b52290852d1b31f277849c58185f59e0a6def
leonwgc <giantfish@126.com>
2022-09-09 13:56:08 +0800

    chore: update button demo

--- demo/Button.tsx
+++ demo/Button.tsx
@@ -9,71 +9,37 @@ export default function App() {
       <DemoBlock title="不同类型的按钮">
         <Space wrap>
           <Button>Default</Button>
-          <Button outlined>Outlined</Button>
           <Button type="primary">Primary</Button>
-          <Button danger>Danger</Button>
+          <Button outlined>Outlined</Button>
         </Space>
       </DemoBlock>
 
-      <DemoBlock title="块级按钮">
+      <DemoBlock title="块级按钮 (block)">
         <Space direction="vertical" style={{ width: '100%' }}>
-          <Button block>Block default</Button>
+          <Button block>Block</Button>
           <Button block type="primary">
-            Block primary
-          </Button>
-          <Button block danger dashed>
-            Block danger
+            Block
           </Button>
         </Space>
       </DemoBlock>
 
-      <DemoBlock title="加载状态">
+      <DemoBlock title="加载状态 (loading)">
         <Space wrap>
-          <Button loading>加载中</Button>
-          <Button type="primary" loading>
-            加载中
-          </Button>
+          <Button type="primary" loading></Button>
+          <Button loading></Button>
+          <Button loading>loading</Button>
         </Space>
       </DemoBlock>
 
-      <DemoBlock title="带图标的按钮">
+      <DemoBlock title="带图标的按钮 (icon)">
         <Space wrap>
-          <Button>
-            <Space>
-              <Icon type="uc-icon-sousuo" />
-              <span>搜索</span>
-            </Space>
-          </Button>
-          <Button circle>
-            <Icon type="uc-icon-sousuo" />
-          </Button>
-
-          <Button circle type="primary">
-            <Icon type="uc-icon-sousuo" />
-          </Button>
-
-          <Button>
-            <Icon type="uc-icon-sousuo" />
-          </Button>
+          <Button circle type="primary" icon={<Icon type="uc-icon-sousuo" />}></Button>
+          <Button icon={<Icon type="uc-icon-sousuo" />}></Button>
+          <Button circle icon={<Icon type="uc-icon-sousuo" />}></Button>
         </Space>
       </DemoBlock>
 
-      <DemoBlock title="按钮形状">
-        <Space wrap>
-          <Button>Default Button</Button>
-          <Button block style={{ borderRadius: 20 }}>
-            Rounded Button
-          </Button>
-          <Button block style={{ borderRadius: 0 }}>
-            Rect Button
-          </Button>
-          <Button circle>
-            <Icon type="uc-icon-guanbi" />
-          </Button>
-        </Space>
-      </DemoBlock>
-
-      <DemoBlock title="幽灵按钮" background="#333">
+      <DemoBlock title="幽灵按钮 (ghost)" background="#333">
         <Space direction="vertical" style={{ width: '100%' }}>
           <Button block ghost>
             default
@@ -82,15 +48,11 @@ export default function App() {
           <Button block ghost type="primary">
             primary
           </Button>
-
-          <Button ghost block danger dashed>
-            danger dashed
-          </Button>
         </Space>
       </DemoBlock>
 
-      <DemoBlock title="链接按钮">
-        <Space wrap size={16}>
+      <DemoBlock title="链接按钮 (as='a')">
+        <Space wrap>
           <Button as="a">Anchor</Button>
           <Button as="a" style={{ color: '#333', textDecoration: 'underline' }}>
             Anchor
@@ -101,20 +63,16 @@ export default function App() {
         </Space>
       </DemoBlock>
 
-      <DemoBlock title="禁用状态">
+      <DemoBlock title="禁用状态 (disabled)">
         <Space wrap>
           <Button disabled>Disabled</Button>
           <Button disabled type="primary">
             Disabled
           </Button>
-
-          <Button disabled danger>
-            Disabled
-          </Button>
         </Space>
       </DemoBlock>
 
-      <DemoBlock title="防止重复click">
+      <DemoBlock title="防止重复点击 (wait)">
         <Space>
           <Button wait>1s点一次</Button>
 
