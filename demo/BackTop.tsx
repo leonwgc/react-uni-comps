9b4dcdd1c2daca6517fd3c9168f5c090d33b066d
leonwgc <giantfish@126.com>
2023-01-03 16:07:55 +0800

    chore: update backtop demo

--- demo/BackTop.tsx
+++ demo/BackTop.tsx
@@ -1,81 +1,19 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React from 'react';
 import PageWrap from './common/PageWrap';
 import DemoBlock from './common/DemoBlock';
-import { Button, Pullup, BackTop, Cell, PullToRefresh } from 'react-uni-comps';
-
-const pageSize = 30;
+import { BackTop, Icon, FloatingBubble } from 'react-uni-comps';
 
 const App = () => {
-  const [list, setList] = useState([]);
-  const [finished, setFinished] = useState(false);
-  const ref = useRef(0);
-
-  const fetchData = useCallback(() => {
-    return new Promise((resolve) => {
-      var ar = [];
-      for (var i = 0; i < pageSize; i++) {
-        ar.push(ref.current * pageSize + i + 1);
-      }
-      setTimeout(() => {
-        setList((d) => d.concat(ar));
-        ref.current++;
-
-        console.log(ref.current);
-
-        if (ref.current > 3) {
-          setFinished(true);
-        }
-        resolve(0);
-      }, 1000);
-    });
-  }, []);
-
-  const onRefresh = useCallback(() => {
-    return new Promise((resolve) => {
-      ref.current = 0;
-      setFinished(false);
-      var ar = [];
-      const randomChar = Math.random().toString().slice(2, 4);
-      for (var i = 0; i < pageSize; i++) {
-        ar.push(i + 1 + '-' + randomChar);
-      }
-      setTimeout(() => {
-        setList(ar);
-
-        resolve(0);
-      }, 1000);
-    });
-  }, []);
-
   return (
-    <PageWrap style={{ padding: 0 }}>
-      <DemoBlock title="下滑100px显示">
-        <PullToRefresh onRefresh={onRefresh}>
-          <Pullup
-            dataList={list}
-            fetchData={fetchData}
-            finished={finished}
-            dataRender={(data) => <Cell title={`item${data}`} />}
-          />
-        </PullToRefresh>
-      </DemoBlock>
+    <PageWrap>
+      <div style={{ height: '200vh', background: '#fff', padding: 20 }}>
+        向下滚动100px显示回到顶部按钮
+      </div>
 
       <BackTop>
-        <Button
-          circle
-          style={{
-            width: 60,
-            height: 60,
-            position: 'fixed',
-            bottom: 60,
-            right: 20,
-            fontSize: 20,
-            color: '#8c8c8c',
-            boxShadow: '0 2px 12px 0 rgba(0, 0, 0, 0.1)',
-          }}
-        >
-          Top
-        </Button>
+        <FloatingBubble style={{ bottom: 45, right: 45, left: 'unset' }}>
+          <Icon type="uc-icon-xiangxia" style={{ transform: 'rotate(-180deg)', fontSize: 24 }} />
+        </FloatingBubble>
       </BackTop>
     </PageWrap>
   );
