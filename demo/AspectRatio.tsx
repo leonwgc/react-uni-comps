9cd02e5b6feb87dc2738c85b5882a8626d92c5f6
leonwgc <giantfish@126.com>
2023-01-18 15:57:26 +0800

    docs: demo

--- demo/AspectRatio.tsx
+++ demo/AspectRatio.tsx
@@ -3,19 +3,19 @@ import PageWrap from './common/PageWrap';
 import DemoBlock from './common/DemoBlock';
 import { AspectRatio } from 'react-uni-comps';
 
-const img = 'https://t7.baidu.com/it/u=2808499218,4024510450&fm=193&f=GIF';
+const img = 'https://img0.baidu.com/it/u=3844111768,3886767277&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500';
 
 export default function App() {
   return (
     <PageWrap>
-      <DemoBlock title="15/9" padding={0}>
+      <DemoBlock title="aspect-ratio: 15 / 9">
         <AspectRatio ratio={15 / 9}>
           <img src={img} />
         </AspectRatio>
       </DemoBlock>
 
-      <DemoBlock title="4/3" padding={0}>
-        <AspectRatio>
+      <DemoBlock title="aspect-ratio: 2 / 1">
+        <AspectRatio ratio={2 / 1}>
           <img src={img} />
         </AspectRatio>
       </DemoBlock>
--- demo/Mask.tsx
+++ demo/Mask.tsx
@@ -4,7 +4,7 @@ import Block from './common/DemoBlock';
 import { Button, Mask, Space } from 'react-uni-comps';
 
 export default function App() {
-  const [v, setV] = useState(false);
+  const [v, setV] = useState(true);
   const [v1, setV1] = useState(false);
   const [v2, setV2] = useState(false);
 
@@ -12,19 +12,13 @@ export default function App() {
     <PageWrap>
       <Block>
         <Space>
-          <Button type="primary" onClick={() => setV(true)}>
-            默认
-          </Button>
+          <Button onClick={() => setV(true)}>默认</Button>
           <Mask visible={v} onClick={() => setV(false)} />
 
-          <Button type="primary" onClick={() => setV1(true)}>
-            颜色
-          </Button>
+          <Button onClick={() => setV1(true)}>颜色</Button>
           <Mask visible={v1} onClick={() => setV1(false)} style={{ background: 'red' }} />
 
-          <Button type="primary" onClick={() => setV2(true)}>
-            透明度和过度效果
-          </Button>
+          <Button onClick={() => setV2(true)}>透明度和动画</Button>
           <Mask opacity={0.7} duration={100} visible={v2} onClick={() => setV2(false)} />
         </Space>
       </Block>
