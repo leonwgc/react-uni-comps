daa8868d34f5dcfef930f93fe25a9cddbf2b63ca
leonwgc <giantfish@126.com>
2023-01-21 11:27:56 +0800

    chore: desc & deps update

--- demo/index.tsx
+++ demo/index.tsx
@@ -7,10 +7,6 @@ import App from './App';
 const initialState = { theme: '#005cff' };
 const store = configureStore(initialState, true);
 
-// import VConsole from 'vconsole';
-
-// const vConsole = new VConsole();
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
